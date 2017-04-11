e139ec0632f0c815b3cd2099f75b82a14632f162
rbohmert <bohmrube@gmail.com>
2017-04-11 03:24:16 +0200

    Update launcher.js
    up

--- launcher.js
+++ launcher.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
-script.src = "https://rawgit.com/jbatea/Road_to_victory/master/autoscript.js";
+script.src = "https://rawgit.com/rbohmert/colorspace/master/autoscript.js";
 script.onload = function () {
     var images = [
         {
