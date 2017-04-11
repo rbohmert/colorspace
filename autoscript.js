3b6de1fa4940c614f7a91395e96ea1b77b50b33f
rbohmert <bohmrube@gmail.com>
2017-04-11 21:07:27 +0200

    Update autoscript.js

--- autoscript.js
+++ autoscript.js
@@ -1,36 +1,75 @@
-function AutoPXLS(images){
-
-	var Painter = function(config){
-		var board = document.getElementById("canvas").getContext('2d'); //colorthis.space
-		// var board = document.getElementById("board").getContext('2d'); //pxls.space
-		var title = config.title || "unnamed";
-
-		var img = new Image();
-		img.crossOrigin = "anonymous";
-		img.src = config.image;
-		var x = config.x;
-		var y = config.y;
-
+function ChanclaTyper(X,Y,imge){
+    //console.log("script loaded");
+	
+	var startX = X;
+	var startY = Y;
+	
+	var image = new Image(); // or document.createElement('img'); 
+	var arrzy
+	image.onload = function(){
+		console.log("AYAAAA INITIALIZING");
 		var canvas = document.createElement('canvas');
-		var image;
-
-		var image_loaded_flag = false;
+		var ctx = canvas.getContext("2d");
+		canvas.width = image.width;
+		canvas.height = image.height;
+		//alert(canvas.width +","+canvas.height);
+		ctx.drawImage(image, 0, 0, image.width, image.height);
 		
-
-		function isSamePixelColor(coords){
-			var board_pixel = board.getImageData((parseInt(x) + parseInt(coords["x"])), (parseInt(y) + parseInt(coords["y"])), 1, 1).data;
-			var image_pixel = getNearestColor(image.getImageData(coords["x"], coords["y"], 1, 1).data);
-			console.log(" color on x: %i y : %i, skip",(parseInt(x) + parseInt(coords["x"])),(parseInt(y) + parseInt(coords["y"])));
+		var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);	
+		var i;
+		var temparray = [];
+		for (i = 0; i < imgData.data.length; i += 4) {
+			imgData.data[i] = imgData.data[i];
+			imgData.data[i+1] = imgData.data[i+1];
+			imgData.data[i+2] = imgData.data[i+2];
+			imgData.data[i+3] = 255;
 			
-	console.log(board_pixel + "et" + image_pixel)
-			//if(image_pixel[3] <= 127) return true;
-			for(var i = 0; i < 3; i++){
-				if(board_pixel[i] != image_pixel[i]) return false;
-			}
-			return true;
+			temparray.push(RGBAtoPixel(imgData.data));
 		}
+		
+		// Tableau
+		// height(lignes) puis width(colonnes)
 
-		function getNearestColor(pixel){	
+		var appVar = [];
+		var tempvar = [];
+		var count = 0;
+		for (l = 0; l < canvas.height ; l++)
+		{
+			tempvar = [];
+			for (c = 0 ; c < canvas.width ; c++)
+			{
+				tempvar.push(temparray[count]);
+				count++;
+			}
+			appVar.push(tempvar);
+		}
+		
+		var script = document.createElement('script');
+		script.src = "https://rawgit.com/Jarts37/ColorBotYatangaki/master/ColorWriter.js";
+		script.onload = function () {
+			ColorWriter(appVar,startX,startY);
+		};
+		document.head.appendChild(script);
+		
+		// TEST TABLEAU
+		/*
+		var arr =appVar,arrText='';
+
+				for (var i = 0; i < arr.length; i++) {
+					for (var j = 0; j < arr[i].length; j++) {
+						arrText+=arr[i][j]+' ';
+					}
+					console.log(arrText);
+					arrText='';
+				}
+		*/
+		
+	}
+	image.crossOrigin = 'Anonymous';
+	image.src = imge;
+	
+	function RGBAtoPixel(pixel){	
+			
 			var colors = [
 				[255,255,255],
 				[228,228,228],
@@ -67,126 +106,6 @@ function AutoPXLS(images){
 						tmp_dif = dif;
 					}
 				}
-				return (colors[color_id]);
+				return (color_id);
 		}
-
-		function getColorId(coords){
-			var pixel = image.getImageData(coords["x"], coords["y"], 1, 1).data;
-			var colors = [
-				[255,255,255],
-				[228,228,228],
-				[136,136,136],
-				[34,34,34],
-				[255,167,209],
-				[229,0,0],
-				[229,149,0],
-				[160,106,66],
-				[229,217,0],
-				[148,224,68],
-				[2,190,1],
-				[0,211,221],
-				[0,131,199],
-				[0,0,234],
-				[207,110,228],
-				[130,0,128]
-			];
-
-				var color_id = -1;
-				var flag = false;
-				for(var i = 0; i < colors.length; i++){
-					flag = true;
-					for(var j = 0; j < 3; j++){
-						if(pixel[j] != colors[i][j]){
-							flag = false;
-							break;
-						}
-					}
-					if(flag){
-						color_id = i;
-						break;
-					}
-				}
-				if (color_id === -1){
-					color_id = getNearestColor(pixel);
-				}
-				return color_id;
-		}
-
-		function pictureIsDone(){
-			for(var _x = 0; _x < canvas.width; _x++){
-				for(var _y = 0; _y < canvas.height; _y++) {
-					var coords = {x: _x, y: _y};
-					if(!isSamePixelColor(coords))
-						return 0;
-				}
-			}
-			return 1;
-		}
-		function tryToDraw(){
-			while (!pictureIsDone())
-			{
-				var coords = {x: Math.floor(Math.random() * canvas.width), y: Math.floor(Math.random() * canvas.height)};
-				if(isSamePixelColor(coords)){
-					console.log("same color on x: %i y : %i, skip",(parseInt(x) + parseInt(coords["x"])),(parseInt(y) + parseInt(coords["y"])));
-				}
-				else{
-					console.log("drawing " + title + " coords " + " x:" + (parseInt(x) + parseInt(coords["x"])) + " y:" + (parseInt(y) + parseInt(coords["y"])));
-					//App.switchColor(getColorId(coords)); //pxls.space
-					tryColorPixel(parseInt(parseInt(x) + parseInt(coords["x"])), (parseInt(y) + parseInt(coords["y"])), getColorId(coords)); //colorthis.space
-					//App.attemptPlace ( (parseInt(x) + parseInt(coords["x"])), (parseInt(y) + parseInt(coords["y"])) ); //pxls.space
-					return 1;
-				}
-			}
-			console.log(title + " is correct");
-			return -1;
-		}
-
-		function drawImage(){
-			if(image_loaded_flag){
-				return tryToDraw();
-			}
-			return -1;
-		}
-
-		function isReady(){
-			return image_loaded_flag;
-		}
-
-		img.onload = function(){
-			canvas.width = img.width;
-			canvas.height = img.height;
-			image = canvas.getContext('2d');
-			image.drawImage(img, 0, 0, img.width, img.height);
-
-			image_loaded_flag = true;
-		};
-
-
-
-		return {
-drawImage: drawImage,
-		   isReady: isReady
-		}
-	};
-
-
-	var painters = [];
-	for(var i = 0; i < images.length; i++){
-		painters[i] = Painter(images[i]);
-	}
-
-	function draw(){
-			for(var i = 0; i < painters.length; i++){
-				if(painters[i].isReady()){
-					var result = painters[i].drawImage();
-					if(result > 0){
-						setTimeout(draw, 5000);
-						return;
-					}
-				}
-			}
-			setTimeout(draw, 1000);
-		return;
-	}
-	draw();
 }
