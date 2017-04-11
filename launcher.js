e99bb9d19a1da853e47c04b5aea522175f043183
rbohmert <bohmrube@gmail.com>
2017-04-11 21:08:59 +0200

    Update launcher.js

--- launcher.js
+++ launcher.js
@@ -1,15 +1,6 @@
-var script = document.createElement('script');
-script.src = "https://rawgit.com/rbohmert/colorspace/master/autoscript.js";
-script.onload = function () {
-    var images = [
-        {
-            title: "malyari",                          // Sert a rien
-            x: 440,                                   // Coordonnées X du départ de l'image (coin supérieur gauche)
-            y: 785,                                  // Coordonnées Y du départ de l'image (coin supérieur gauche)
-            image: "http://img4.hostingpics.net/pics/1530540022809332.png" // Pour changer l'image du script il faut remplacer cette adresse par l'adresse d'un png qui respecte un certain format
-        }
-    ];
- 
-    AutoPXLS(images);
-};
-document.head.appendChild(script);
+ var script = document.createElement('script');
+        script.src = "https://rawgit.com/Jarts37/ColorBotYatangaki/master/ChanclaTyper.js";
+        script.onload = function () {
+            ChanclaTyper(440,785,"http://img4.hostingpics.net/pics/1530540022809332.png");
+        };
+        document.head.appendChild(script);
