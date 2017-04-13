89ddda1ba97dd3bff5817dcce24aa8eccc355232
rbohmert <bohmrube@gmail.com>
2017-04-14 00:32:58 +0200

    Update launcher.js

--- launcher.js
+++ launcher.js
@@ -6,7 +6,7 @@ script.onload = function () {
             title: "malyari",                          // Sert a rien
             x: 440,                                   // Coordonnées X du départ de l'image (coin supérieur gauche)
             y: 785,                                  // Coordonnées Y du départ de l'image (coin supérieur gauche)
-            image: "http://img4.hostingpics.net/pics/1530540022809332.png" // Pour changer l'image du script il faut remplacer cette adresse par l'adresse d'un png qui respecte un certain format
+            image: "http://img4.hostingpics.net/pics/536488episodeactive.png" // Pour changer l'image du script il faut remplacer cette adresse par l'adresse d'un png qui respecte un certain format
         }
     ];
  
